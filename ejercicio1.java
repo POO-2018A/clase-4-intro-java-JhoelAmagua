89a4e640d0ca0be7324d40b758bd083f9be6f794
JhoelAmagua <38430627+JhoelAmagua@users.noreply.github.com>
2018-04-22 22:23:02 -0500

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,14 @@
+package a;
+
+public class ejercicio1 {
+    public static void main (String []args ){
+
+        double fahrenheit=45.56;
+        double celsius;
+
+        celsius=((fahrenheit-32)*5)/9;
+
+        System.out.println("El valor en fahrenheit es: "+fahrenheit);
+        System.out.println("El valor en celsius es: "+celsius);
+    }
+}
